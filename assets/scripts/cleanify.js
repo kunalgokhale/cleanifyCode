91f4dbd1e7cd22b6f04eb7e424eee3ad69b21944
Kunal Gokhale <kunal02892@gmail.com>
2019-10-02 01:32:54 +0800

    trying code mirror text area

--- assets/scripts/cleanify.js
+++ assets/scripts/cleanify.js
@@ -1,7 +1,12 @@
+function initTextAreas() {
+
+    var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("sourceArea"));
+}
+
 function beautifyJSON() {
     if (IsValidJSONString(document.getElementById("sourceArea").value)){
         var rawCode = JSON.parse(document.getElementById("sourceArea").value);
-        document.getElementById("outputArea").value = JSON.stringify(rawCode, null, 4);
+        document.getElementById("outputArea").value = JSON.stringify(rawCode, null, 3);
     } else {
         alert("Invalid JSON input!");
     }
